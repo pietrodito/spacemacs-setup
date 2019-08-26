31405f7557525c19317fbaeb1d5d32dde268ca01
pietrodito <pietrodito@gmail.com>
2019-08-26 21:10:53 +0200

    first commit

---
+++ run-installers.sh
@@ -0,0 +1,5 @@
+. ./installers/source-to-set-repo-dir-var.sh
+./installers/create-symlink-to-dot-spacemacs.sh
+./installers/copy-zsh-files-to-custom-dir.sh
+./installers/emacs-at-startup.sh
+./installers/home-bin-commands.sh
