cbb2670bad62feaf465dc6b04199c04fa5ee89b1
pietrodito <pietrodito@gmail.com>
2019-10-23 13:58:22 +0200

    Backup

--- helpers/emacs-aliases.zsh
+++ helpers/emacs-aliases.zsh
@@ -1,3 +1,4 @@
 alias m='emacsclient --create-frame --no-wait'
 alias mc='emacsclient --create-frame --no-wait ~/Comp'
 alias mh='emacsclient --create-frame --no-wait `pwd`'
+alias ms='emacsclient --create-frame --no-wait --exec '(spacemacs/default-pop-shell)''
