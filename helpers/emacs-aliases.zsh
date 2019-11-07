31aca5765106de42b5813f665b1f633ca895084c
pietrodito <pietrodito@gmail.com>
2019-11-07 15:12:40 +0100

    minor changes

--- helpers/emacs-aliases.zsh
+++ helpers/emacs-aliases.zsh
@@ -1,4 +1,4 @@
 alias m='emacsclient --create-frame --no-wait'
 alias mc='emacsclient --create-frame --no-wait ~/Comp'
 alias mh='emacsclient --create-frame --no-wait `pwd`'
-alias ms='emacsclient --create-frame --no-wait --exec '(spacemacs/default-pop-shell)''
+alias ms="emacsclient --create-frame --no-wait --eval '(spacemacs/default-pop-shell)'"
